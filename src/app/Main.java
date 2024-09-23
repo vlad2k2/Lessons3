6015f52409fd1c084af1421e107df491e8bd44d4
Vlad <vlad_rvz@ukr.net>
2024-09-23 22:18:58 +0300

    First commit

---
+++ src/app/Main.java
@@ -0,0 +1,13 @@
+package app;
+
+public class Main {
+
+    public static void main(String[] args){
+        char point = '.';
+        String firstCommit = "\"This is shop-online app\"";
+
+        System.out.print(firstCommit + point);
+
+    }
+
+}
